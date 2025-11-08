738345498f5e6c3ebd4c1286d9842deee821548b
Late-Insider <contact@laaate.onmicrosoft.com>
2025-11-08 18:35:13 +0200

    Delete app/newsletter/deep-work-shallow-world/page.tsx

--- app/newsletter/deep-work-shallow-world/page.tsx
+++
@@ -1,5 +0,0 @@
-import { NewsletterClientPage } from "../[slug]/client-page"
-
-export default function DeepWorkShallowWorldPage() {
-  return <NewsletterClientPage params={{ slug: "deep-work-shallow-world" }} />
-}
