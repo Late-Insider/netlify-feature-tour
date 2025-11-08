60e72cbdcd5c0fc9ed1cc7c5712228a08e94d8dd
Late-Insider <contact@laaate.onmicrosoft.com>
2025-11-08 18:38:22 +0200

    Delete app/newsletter/mastering-selective-attention/page.tsx

--- app/newsletter/mastering-selective-attention/page.tsx
+++
@@ -1,5 +0,0 @@
-import { NewsletterClientPage } from "../[slug]/client-page"
-
-export default function MasteringSelectiveAttentionPage() {
-  return <NewsletterClientPage params={{ slug: "mastering-selective-attention" }} />
-}
