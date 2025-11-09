eab27e78480c1f14a1e7d03e899c7f45188670d3
Late-Insider <contact@laaate.onmicrosoft.com>
2025-11-09 20:45:57 +0200

    Delete app/blog/[slug]/page.tsx

--- app/blog/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-// Server redirect to /newsletter
-import { redirect } from "next/navigation"
-
-export const dynamic = "force-dynamic"
-export const revalidate = 0
-
-export default function BlogSlugRedirect() {
-  redirect("/newsletter")
-}
